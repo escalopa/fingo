315c94ea988f94703e5c40e15d625252785f4296
escalopa <ahmad.helaly.dev@gmail.com>
2023-02-18 09:49:37 +0300

    created auth token generator with tests

---
+++ auth/internal/adapters/token/paseto.go
@@ -0,0 +1,58 @@
+package token
+
+import (
+	"time"
+
+	ac "github.com/escalopa/gofly/auth/internal/core"
+	"github.com/lordvidex/errs"
+
+	"github.com/o1egl/paseto"
+)
+
+const minSecretKeyLen = 32
+
+type PasetoTokenizer struct {
+	p   *paseto.V2
+	sk  []byte
+	ate time.Duration // access token expiration time
+}
+
+func NewPaseto(secretKey string, accessTokenDuration time.Duration) (*PasetoTokenizer, error) {
+	if len(secretKey) < minSecretKeyLen {
+		return nil, errs.B().
+			Code(errs.InvalidArgument).
+			Msgf("secretKet len is less than the min value %d", minSecretKeyLen).
+			Err()
+	}
+
+	return &PasetoTokenizer{
+		p:   paseto.NewV2(),
+		sk:  []byte(secretKey),
+		ate: accessTokenDuration,
+	}, nil
+}
+
+func (pt *PasetoTokenizer) GenerateToken(u ac.User) (string, error) {
+	ut := ac.UserToken{
+		User:      u,
+		IssuedAt:  time.Now(),
+		ExpiresAt: time.Now().Add(pt.ate),
+	}
+	token, err := pt.p.Encrypt(pt.sk, ut, nil)
+	if err != nil {
+		return "", err
+	}
+	return token, err
+}
+
+func (pt *PasetoTokenizer) VerifyToken(token string) (ac.User, error) {
+	var ut ac.UserToken
+	err := pt.p.Decrypt(token, pt.sk, &ut, nil)
+	if err != nil {
+		return ac.User{}, err
+	}
+	if time.Now().After(ut.ExpiresAt) {
+		return ac.User{}, errs.B().Code(errs.Unauthenticated).Msg("token expired").Err()
+	}
+	return ut.User, nil
+}
---
+++ auth/internal/adapters/token/paseto_test.go
@@ -0,0 +1,58 @@
+package token
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	ac "github.com/escalopa/gofly/auth/internal/core"
+	"github.com/lordvidex/errs"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPaseto(t *testing.T) {
+	// Create paseto
+	p, err := NewPaseto("12345678901234567890123456789012", 1*time.Minute)
+	require.NoError(t, err)
+	require.NotNil(t, p)
+	// Generate token
+	u, err := randomUser()
+	require.NoError(t, err)
+	token, err := p.GenerateToken(u)
+	require.NoError(t, err)
+	require.NotEmpty(t, t)
+
+	// VerifyToken token
+	u2, err := p.VerifyToken(token)
+	require.NoError(t, err)
+	require.True(t, reflect.DeepEqual(u, u2))
+}
+
+func TestPasetoExpired(t *testing.T) {
+	// Create paseto
+	p, err := NewPaseto("12345678901234567890123456789012", 1*time.Nanosecond)
+	require.NoError(t, err)
+	require.NotNil(t, p)
+	// Generate token
+	u, err := randomUser()
+	require.NoError(t, err)
+	token, err := p.GenerateToken(u)
+	require.NoError(t, err)
+	require.NotEmpty(t, t)
+	// VerifyToken token
+	u2, err := p.VerifyToken(token)
+	require.Error(t, err)
+	require.Empty(t, u2)
+	// Check error
+	errr, ok := err.(*errs.Error)
+	require.True(t, ok)
+	require.Equal(t, []string{"token expired"}, errr.Msg)
+	require.Equal(t, errs.Unauthenticated, errr.Code)
+}
+
+func randomUser() (ac.User, error) {
+	return ac.User{
+		Email:    "test@gmail.com",
+		Password: "123456",
+	}, nil
+}
