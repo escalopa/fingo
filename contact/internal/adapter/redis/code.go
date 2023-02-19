79f60403b9dc17ffa9f7e66f612284ae6e8d68a6
escalopa <ahmad.helaly.dev@gmail.com>
2023-02-19 16:36:05 +0300

    update port with close function

--- contact/internal/adapter/redis/code.go
+++ contact/internal/adapter/redis/code.go
@@ -13,26 +13,37 @@ type CodeRepository struct {
 	exp time.Duration
 }
 
+// NewCodeRepository creates a new code repository
+// The default expiration time is 5 minutes
 func NewCodeRepository(client *redis.Client, opts ...func(*CodeRepository)) *CodeRepository {
-	cr := &CodeRepository{r: client}
+	cr := &CodeRepository{
+		r:   client,
+		c:   context.Background(),
+		exp: 5 * time.Minute,
+	}
 	for _, opt := range opts {
 		opt(cr)
 	}
 	return cr
 }
 
+// WithCodeContext sets the context for the code repository
 func WithCodeContext(ctx context.Context) func(*CodeRepository) {
 	return func(cr *CodeRepository) {
 		cr.c = ctx
 	}
 }
 
+// WithExpiration sets the expiration time for the code
+// The default expiration time is 5 minutes
 func WithExpiration(exp time.Duration) func(*CodeRepository) {
 	return func(cr *CodeRepository) {
 		cr.exp = exp
 	}
 }
 
+// Save saves the code in the redis database for the given `user id`
+// The code is set to expire after `exp` seconds
 func (cr *CodeRepository) Save(code string, userID string) error {
 	err := cr.r.Set(cr.c, code, userID, cr.exp).Err()
 	if err != nil {
@@ -41,13 +52,20 @@ func (cr *CodeRepository) Save(code string, userID string) error {
 	return nil
 }
 
+// Get returns the user id associated with the given code if it exists
 func (cr *CodeRepository) Get(code string) (string, error) {
 	value, err := cr.r.Get(cr.c, code).Result()
 	if err != nil {
+		// if the code does not exist, return a not found error
 		if err == redis.Nil {
 			return "", errs.B(err).Code(errs.NotFound).Msg("code not found").Err()
 		}
+		// otherwise, return an internal error
 		return "", errs.B(err).Code(errs.Internal).Msg("failed to get code").Err()
 	}
 	return value, nil
+}
+
+func (cr *CodeRepository) Close() error {
+	return cr.r.Close()
 }
--- contact/internal/application/ports.go
+++ contact/internal/application/ports.go
@@ -3,4 +3,14 @@ package application
 type CodeRepository interface {
 	Save(code string, userID string) error
 	Get(code string) (string, error)
+	Close() error
+}
+
+type EmailSender interface {
+	SendVerificationCode(email string, code string) error
+	Close() error
+}
+
+type CodeGenerator interface {
+	GenerateCode() string
 }
