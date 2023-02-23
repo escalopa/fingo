151ca6165ac83f57fd5148105984a590cc470fa3
escalopa <ahmad.helaly.dev@gmail.com>
2023-02-23 22:07:05 +0300

    handle pg migration not updated error

--- auth/internal/adapters/db/postgres/db.go
+++ auth/internal/adapters/db/postgres/db.go
@@ -7,6 +7,7 @@ import (
 	"github.com/mattes/migrate"
 	"github.com/mattes/migrate/database/postgres"
 	_ "github.com/mattes/migrate/source/file"
+	"log"
 )
 
 func New(url string) (*sql.DB, error) {
@@ -22,18 +23,21 @@ func Migrate(conn *sql.DB, migrationDir string) error {
 	// Create a new pg instance
 	driver, err := postgres.WithInstance(conn, &postgres.Config{})
 	if err != nil {
-		return err
+		return errs.B(err).Msg("failed to create driver for migration").Err()
 	}
 	// Load migration files
 	m, err := migrate.NewWithDatabaseInstance(
 		migrationDir,
 		"postgres", driver)
 	if err != nil {
-		return err
+		return errs.B(err).Msg("failed to create new pg instance for migration").Err()
 	}
 	// Push migration changes
 	if err = m.Up(); err != nil {
-		return err
+		if err != migrate.ErrNoChange {
+			return err
+		}
+		log.Printf("Databse is up to date, No migration made")
 	}
 	return nil
 }
